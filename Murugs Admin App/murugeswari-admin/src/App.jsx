3d94b43068b5aec10f5f7a655b6ef011245ceed7
Jeevlin <princessjeev@gmail.com>
2026-04-18 17:39:51 +0530

    fix case issue

--- Murugs Admin App/murugeswari-admin/src/App.jsx
+++ Murugs Admin App/murugeswari-admin/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Dashboard from "./pages/dashboard";
 import Orders from "./pages/orders";
 import Inventory from "./pages/inventory";
